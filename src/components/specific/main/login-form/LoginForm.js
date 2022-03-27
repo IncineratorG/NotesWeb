cfe6122cba027f399397cce65f36c008fc98ce61
IncineratorG <alexander.v.dorohov@yandex.ru>
2022-03-27 18:41:15 +0300

    Работа над формой входа.

--- src/components/specific/main/login-form/LoginForm.js
+++ src/components/specific/main/login-form/LoginForm.js
@@ -7,8 +7,14 @@ const LoginForm = () => {
 
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.loginMainContainer} />
-      <div className={styles.passwordMainContainer} />
+      <div className={styles.loginMainContainer}>
+        <div className={styles.loginInnerContainer}>
+          <div className={styles.loginIconContainer} />
+        </div>
+      </div>
+      <div className={styles.passwordMainContainer}>
+        <div className={styles.passwordInnerContainer} />
+      </div>
     </div>
   )
 }
@@ -18,17 +24,34 @@ const componentStyles = {
     display: "flex",
     flexDirection: "column",
     backgroundColor: "red",
-    height: "20vh",
-    width: "30vw",
+    height: "20%",
+    width: "30%",
   },
   loginMainContainer: {
+    display: "flex",
     flex: 1,
     backgroundColor: "coral",
   },
+  loginInnerContainer: {
+    flex: 1,
+    margin: "2%",
+    backgroundColor: "lightgrey",
+  },
+  loginIconContainer: {
+    height: "100%",
+    width: "10%",
+    backgroundColor: "red",
+  },
   passwordMainContainer: {
+    display: "flex",
     flex: 1,
     backgroundColor: "orange",
-  }
+  },
+  passwordInnerContainer: {
+    flex: 1,
+    margin: "2%",
+    backgroundColor: "lightgrey",
+  },
 }
 
 export default React.memo(LoginForm);
